8804cd952178c21b200c8ab7294a06227fe63a24
ayman_moubayed <aymanmoubayed@gmail.com>
2020-12-14 23:45:41 -0500

    GPS and Google Map work
    Gps has autofil location and google maps show results of each

--- Mobile-FINAL/www/js/JS.js
+++ Mobile-FINAL/www/js/JS.js
@@ -49,7 +49,17 @@ async function getInfo(){
  // Selecting the input element and get its value 
             var inputVal = document.getElementById("input").value;
             var inputVal2 = document.getElementById("input2").value;
-            
+            var res = inputVal.split(",");
+            var lat = res[0];
+            var lon=res[1];
+    console.log(lat);
+    console.log(lon);
+            if (inputVal.length < 28){
+                url='https://api.yelp.com/v3/businesses/search?term='+inputVal2+'&location='+inputVal;
+            }
+            else{
+                url='https://api.yelp.com/v3/businesses/search?term='+inputVal+'&latitude='+lat+'&longitude='+lon;
+            }
 
         
            var token = "139SiL9OvyPI6q9jBUU5Kksv8a_P30YzcesJb3Phumcww10-ss3ymhnIm_CfeGeFB5q6bZo1GNv2fjdgTaLQfZNiMEwzBZCQpQV_lljWE50dGYLIq2yW1_zGX-fTX3Yx";
@@ -68,64 +78,81 @@ async function getInfo(){
                     document.getElementById("rating1").textContent=(displayinfo.businesses[0].review_count);
                     document.getElementById("review_count1").textContent=(displayinfo.businesses[0].rating);
                      var distance=displayinfo.businesses[0].distance;
-                    document.getElementById("distance1").textContent=(parseInt(distance));
+                    document.getElementById("distance1").textContent=(Math.round(parseInt(distance)/1609));
                     var adress=displayinfo.businesses[0].location.address1+", "+displayinfo.businesses[0].location.city+", "+displayinfo.businesses[0].location.zip_code;
                     document.getElementById("adress1").textContent=(adress);
                     document.getElementById("cat1").textContent=(displayinfo.businesses[0].categories[0].title);
                     document.getElementById("p_number1").textContent=(displayinfo.businesses[0].display_phone);
                     document.getElementById("avalible_for1").textContent=(displayinfo.businesses[0].transactions);
+                    var coords = displayinfo.businesses[0].coordinates.latitude+','+displayinfo.businesses[0].coordinates.longitude;
+                    document.getElementById("location1").textContent=(coords);
+                    localStorage.setItem('location1', coords);    
                         
                     document.getElementById("name2").textContent=(displayinfo.businesses[1].name);
                     document.getElementById("rating2").textContent=(displayinfo.businesses[1].review_count);
                     document.getElementById("review_count2").textContent=(displayinfo.businesses[1].rating);
                      var distance=displayinfo.businesses[1].distance;
-                    document.getElementById("distance2").textContent=(parseInt(distance));
+                    document.getElementById("distance2").textContent=(Math.round(parseInt(distance)/1609));
                     var adress=displayinfo.businesses[1].location.address1+", "+displayinfo.businesses[1].location.city+", "+displayinfo.businesses[1].location.zip_code;
                     document.getElementById("adress2").textContent=(adress);
                     document.getElementById("cat2").textContent=(displayinfo.businesses[1].categories[0].title);
                     document.getElementById("p_number2").textContent=(displayinfo.businesses[1].display_phone);
                     document.getElementById("avalible_for2").textContent=(displayinfo.businesses[1].transactions);    
+                    var coords = displayinfo.businesses[1].coordinates.latitude+','+displayinfo.businesses[1].coordinates.longitude;
+                    document.getElementById("location2").textContent=(coords);
+                    localStorage.setItem('location2', coords);    
+                        
                         
                     document.getElementById("name3").textContent=(displayinfo.businesses[2].name);
                     document.getElementById("rating3").textContent=(displayinfo.businesses[2].review_count);
                     document.getElementById("review_count3").textContent=(displayinfo.businesses[2].rating);
                     var distance=displayinfo.businesses[2].distance;
-                    document.getElementById("distance3").textContent=(parseInt(distance));
+                    document.getElementById("distance3").textContent=(Math.round(parseInt(distance)/1609));
                     var adress=displayinfo.businesses[2].location.address1+", "+displayinfo.businesses[2].location.city+", "+displayinfo.businesses[2].location.zip_code;
                     document.getElementById("adress3").textContent=(adress);
                    document.getElementById("cat3").textContent=(displayinfo.businesses[2].categories[0].title);
                     document.getElementById("p_number3").textContent=(displayinfo.businesses[2].display_phone);
                     document.getElementById("avalible_for3").textContent=(displayinfo.businesses[2].transactions);   
+                    
+                    var coords = displayinfo.businesses[2].coordinates.latitude+','+displayinfo.businesses[2].coordinates.longitude;
+                    document.getElementById("location3").textContent=(coords);
+                    localStorage.setItem('location3', coords);    
+                        
                         
                      document.getElementById("name4").textContent=(displayinfo.businesses[3].name);
                     document.getElementById("rating4").textContent=(displayinfo.businesses[3].review_count);
                     document.getElementById("review_count4").textContent=(displayinfo.businesses[3].rating);
                     var distance=displayinfo.businesses[3].distance;
-                    document.getElementById("distance4").textContent=(parseInt(distance));
+                    document.getElementById("distance4").textContent=(Math.round(parseInt(distance)/1609));
                     var adress=displayinfo.businesses[3].location.address1+", "+displayinfo.businesses[3].location.city+", "+displayinfo.businesses[3].location.zip_code;
                     document.getElementById("adress4").textContent=(adress);
                     document.getElementById("cat4").textContent=(displayinfo.businesses[3].categories[0].title);
                     document.getElementById("p_number4").textContent=(displayinfo.businesses[3].display_phone);
                     document.getElementById("avalible_for4").textContent=(displayinfo.businesses[3].transactions);       
-                        
+                    
+                    var coords = displayinfo.businesses[3].coordinates.latitude+','+displayinfo.businesses[3].coordinates.longitude;
+                    document.getElementById("location4").textContent=(coords);
+                    localStorage.setItem('location4', coords);        
                         
                      document.getElementById("name5").textContent=(displayinfo.businesses[4].name);
                     document.getElementById("rating5").textContent=(displayinfo.businesses[4].review_count);
                     document.getElementById("review_count5").textContent=(displayinfo.businesses[4].rating);
                     var distance=displayinfo.businesses[4].distance;
-                    document.getElementById("distance5").textContent=(parseInt(distance));
+                    document.getElementById("distance5").textContent=(Math.round(parseInt(distance)/1609));
                     var adress=displayinfo.businesses[4].location.address1+", "+displayinfo.businesses[4].location.city+", "+displayinfo.businesses[4].location.zip_code;
                     document.getElementById("adress5").textContent=(adress);
                     document.getElementById("cat5").textContent=(displayinfo.businesses[4].categories[0].title);
                     document.getElementById("p_number5").textContent=(displayinfo.businesses[4].display_phone);
                     document.getElementById("avalible_for5").textContent=(displayinfo.businesses[4].transactions);      
-                        
+                     var coords = displayinfo.businesses[4].coordinates.latitude+','+displayinfo.businesses[4].coordinates.longitude;
+                    document.getElementById("location5").textContent=(coords);
+                    localStorage.setItem('location5', coords);    
          
                     }
                 
     };
           
-            xhr.open('GET', 'https://api.yelp.com/v3/businesses/search?term='+inputVal2+'&location='+inputVal);  
+            xhr.open('GET', url);  
 
     
             xhr.setRequestHeader("Authorization", "Bearer " + token);
@@ -145,136 +172,24 @@ function getcords(){
     
      navigator.geolocation.getCurrentPosition(
       function(position) {
-        var  lat=position.coords.latitude;
-       var   lon=position.coords.longitude;
-           lat=lat.toFixed(3);
-            lon=lon.toFixed(3);
-          lat=lat.toString();
-          lon=lon.toString();
-          var inputVal2 = document.getElementById("input2").value;
-          var url = 'https://api.yelp.com/v3/businesses/search?term='+inputVal2+'&latitude='+lat+'&longitude='+lon;
-          
-          
+        var  lat = String(position.coords.latitude);
+       var   lon = String(position.coords.longitude);
+        
+       document.getElementById('input').value = position.coords.latitude+','+position.coords.longitude;
           
 },
       function() {
           alert('Error getting location');
       }); 
     
-       return url;
+       
           
         
           
 }
       
          
-         
-         
-              
-async function getLoc() {
 
-    
-
-            
-           try{
-               navURL = getcords()
-            
-           
-          
-
-        
-           var token = "139SiL9OvyPI6q9jBUU5Kksv8a_P30YzcesJb3Phumcww10-ss3ymhnIm_CfeGeFB5q6bZo1GNv2fjdgTaLQfZNiMEwzBZCQpQV_lljWE50dGYLIq2yW1_zGX-fTX3Yx";
-                
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.response);
-                    document.getElementById("content").innerHTML = "";
-                    var data = document.createElement('p');
-                    data.innerText = this.response;
-                    displayinfo=JSON.parse(this.response);
-                  
-                    
-                    document.getElementById("name1").textContent=(displayinfo.businesses[0].name);
-                    document.getElementById("rating1").textContent=(displayinfo.businesses[0].review_count);
-                    document.getElementById("review_count1").textContent=(displayinfo.businesses[0].rating);
-                     var distance=displayinfo.businesses[0].distance;
-                    document.getElementById("distance1").textContent=(parseInt(distance));
-                    var adress=displayinfo.businesses[0].location.address1+", "+displayinfo.businesses[0].location.city+", "+displayinfo.businesses[0].location.zip_code;
-                    document.getElementById("adress1").textContent=(adress);
-                    document.getElementById("cat1").textContent=(displayinfo.businesses[0].categories[0].title);
-                    document.getElementById("p_number1").textContent=(displayinfo.businesses[0].display_phone);
-                    document.getElementById("avalible_for1").textContent=(displayinfo.businesses[0].transactions);
-                        
-                    document.getElementById("name2").textContent=(displayinfo.businesses[1].name);
-                    document.getElementById("rating2").textContent=(displayinfo.businesses[1].review_count);
-                    document.getElementById("review_count2").textContent=(displayinfo.businesses[1].rating);
-                     var distance=displayinfo.businesses[1].distance;
-                    document.getElementById("distance2").textContent=(parseInt(distance));
-                    var adress=displayinfo.businesses[1].location.address1+", "+displayinfo.businesses[1].location.city+", "+displayinfo.businesses[1].location.zip_code;
-                    document.getElementById("adress2").textContent=(adress);
-                    document.getElementById("cat2").textContent=(displayinfo.businesses[1].categories[0].title);
-                    document.getElementById("p_number2").textContent=(displayinfo.businesses[1].display_phone);
-                    document.getElementById("avalible_for2").textContent=(displayinfo.businesses[1].transactions);    
-                        
-                    document.getElementById("name3").textContent=(displayinfo.businesses[2].name);
-                    document.getElementById("rating3").textContent=(displayinfo.businesses[2].review_count);
-                    document.getElementById("review_count3").textContent=(displayinfo.businesses[2].rating);
-                    var distance=displayinfo.businesses[2].distance;
-                    document.getElementById("distance3").textContent=(parseInt(distance));
-                    var adress=displayinfo.businesses[2].location.address1+", "+displayinfo.businesses[2].location.city+", "+displayinfo.businesses[2].location.zip_code;
-                    document.getElementById("adress3").textContent=(adress);
-                   document.getElementById("cat3").textContent=(displayinfo.businesses[2].categories[0].title);
-                    document.getElementById("p_number3").textContent=(displayinfo.businesses[2].display_phone);
-                    document.getElementById("avalible_for3").textContent=(displayinfo.businesses[2].transactions);   
-                        
-                     document.getElementById("name4").textContent=(displayinfo.businesses[3].name);
-                    document.getElementById("rating4").textContent=(displayinfo.businesses[3].review_count);
-                    document.getElementById("review_count4").textContent=(displayinfo.businesses[3].rating);
-                    var distance=displayinfo.businesses[3].distance;
-                    document.getElementById("distance4").textContent=(parseInt(distance));
-                    var adress=displayinfo.businesses[3].location.address1+", "+displayinfo.businesses[3].location.city+", "+displayinfo.businesses[3].location.zip_code;
-                    document.getElementById("adress4").textContent=(adress);
-                    document.getElementById("cat4").textContent=(displayinfo.businesses[3].categories[0].title);
-                    document.getElementById("p_number4").textContent=(displayinfo.businesses[3].display_phone);
-                    document.getElementById("avalible_for4").textContent=(displayinfo.businesses[3].transactions);       
-                        
-                        
-                     document.getElementById("name5").textContent=(displayinfo.businesses[4].name);
-                    document.getElementById("rating5").textContent=(displayinfo.businesses[4].review_count);
-                    document.getElementById("review_count5").textContent=(displayinfo.businesses[4].rating);
-                    var distance=displayinfo.businesses[4].distance;
-                    document.getElementById("distance5").textContent=(parseInt(distance));
-                    var adress=displayinfo.businesses[4].location.address1+", "+displayinfo.businesses[4].location.city+", "+displayinfo.businesses[4].location.zip_code;
-                    document.getElementById("adress5").textContent=(adress);
-                    document.getElementById("cat5").textContent=(displayinfo.businesses[4].categories[0].title);
-                    document.getElementById("p_number5").textContent=(displayinfo.businesses[4].display_phone);
-                    document.getElementById("avalible_for5").textContent=(displayinfo.businesses[4].transactions);      
-                        
-         
-                    }
-                
-    };
-          
-            xhr.open('GET', navURL);  
-
-    
-            xhr.setRequestHeader("Authorization", "Bearer " + token);
-            xhr.send();
-                
-        var x = document.getElementById("input1");
-        x.style.display = "none"; 
-        var y= document.getElementById("output");
-        y.style.display = "block"; 
-           }
-    catch(err){
-            alert("no");
-    }
-    
-   
-    
-}
-   
 
       
 
--- Mobile-FINAL/www/js/map.js
+++ Mobile-FINAL/www/js/map.js
@@ -1,24 +1,55 @@
-var showmap = function(){   
 
- var map= document.getElementById("Frame");
- map.style.display="block";
-
-    
-    
-    }
 var domap = function() { //This function should be used to build the map in the content element
-    
-    //TODO : ADD MAP CONTENT
+   
     back();// restores weather app
     var x = document.getElementById("weather app"); //hides weather app
     x.style.display = "none";
     let workspace = document.getElementById("content");
     workspace.innerHTML = "";
-   
+  
    let h1 = document.createElement('h1');
-                let text = document.createTextNode("Please Use Weather app first before viewing map");
+                let text = document.createTextNode("Please Use Yelp app first before viewing map");
                 h1.appendChild(text);
                 workspace.append(h1);
-    showmap();
+    
+    let ifrm = document.createElement('iframe');
+    ifrm.setAttribute('width', '800');
+    ifrm.setAttribute('height', '1000');   
+    
+    let ifrm2 = document.createElement('iframe');
+    ifrm2.setAttribute('width', '800');
+    ifrm2.setAttribute('height', '1000');  
+    
+    let ifrm3 = document.createElement('iframe');
+    ifrm3.setAttribute('width', '800');
+    ifrm3.setAttribute('height', '1000');  
+    
+    let ifrm4 = document.createElement('iframe');
+    ifrm4.setAttribute('width', '800');
+    ifrm4.setAttribute('height', '1000'); 
+    
+    let ifrm5 = document.createElement('iframe');
+    ifrm5.setAttribute('width', '800');
+    ifrm5.setAttribute('height', '1000'); 
+    
+    
+    var location1 = localStorage.getItem('location1');
+    ifrm.setAttribute('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMc9eQ-xBwLJCF03YOAm6N1bZzCOy9RjQ&q="+location1);  
+    workspace.append(ifrm);
+    
+    var location2 = localStorage.getItem('location2');
+    ifrm2.setAttribute('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMc9eQ-xBwLJCF03YOAm6N1bZzCOy9RjQ&q="+location2);  
+    workspace.append(ifrm2);
+    
+    var location3 = localStorage.getItem('location3');
+    ifrm3.setAttribute('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMc9eQ-xBwLJCF03YOAm6N1bZzCOy9RjQ&q="+location3);  
+    workspace.append(ifrm3);
+    
+    var location4 = localStorage.getItem('location4');
+    ifrm4.setAttribute('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMc9eQ-xBwLJCF03YOAm6N1bZzCOy9RjQ&q="+location4);  
+    workspace.append(ifrm4);
+    
+    var location5 = localStorage.getItem('location5');
+    ifrm5.setAttribute('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBMc9eQ-xBwLJCF03YOAm6N1bZzCOy9RjQ&q="+location5);  
+    workspace.append(ifrm5);
 }
-
